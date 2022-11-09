7598c9cf927bcfcbe47337e6667bde386eb504c4
Virendra kumar dwivedi <101566111+Virendrakumardwivedi@users.noreply.github.com>
2022-11-09 22:29:33 +0530

    new add

---
+++ BusReservationPortalSystem/src/test/java/com/bookingportal/BusReservationPortalSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookingportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusReservationPortalSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
