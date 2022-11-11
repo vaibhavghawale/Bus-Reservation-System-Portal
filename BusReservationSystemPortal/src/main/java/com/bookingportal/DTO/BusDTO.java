349e7e8d81c46c26ab5c490acb9cbee836b394bc
RiyaNegi03 <riyanegi7997@gmail.com>
2022-11-11 15:49:02 +0530

    completed all tasks

---
+++ BusReservationSystemPortal/src/main/java/com/bookingportal/DTO/BusDTO.java
@@ -0,0 +1,26 @@
+package com.bookingportal.DTO;
+
+
+import org.springframework.format.annotation.DateTimeFormat;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BusDTO {
+
+	private Integer busId;
+	private String routeFrom;
+	private String routeTo;
+	
+	
+	
+	@JsonFormat(pattern="yyyy-MM-dd")
+	@DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+	private LocalDate busJourneyDate;
+	
+}
